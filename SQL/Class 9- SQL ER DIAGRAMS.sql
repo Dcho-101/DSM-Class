fbb1577f91ee70bc13ab57941610bf8f22da6d7e
Dibyendu Chowdhury <103022440+Dcho-101@users.noreply.github.com>
2024-03-04 09:56:39 +0530

    Add files via upload

---
+++ SQL/Class 9- SQL ER DIAGRAMS.sql
@@ -0,0 +1,23 @@
+-- ER diagrams are used to design database
+
+-- ER diagrams helps others to understand the schema
+
+-- In ER diagram, Squares represemt the table or entity names
+
+--   Circles represent the features ofr columns or attributes of the table
+
+-- underlines represent the primary key
+
+-- there are 4 types of cardinality
+-- 0 to 1, 1 to 1, 1 to many, many to many, 
+
+-- Normalization is used to 
+-- 1. easy acess of the data
+-- 2. remove reduntancy
+-- 3. more data integrity
+
+-- 1 NF (first normal form)
+-- no multiple entries in a single cell (atomic data in cell)
+
+
+
